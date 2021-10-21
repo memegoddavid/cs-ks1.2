4cdcbd5806227ddd5acd478fc7d415a4179abe15
David <68856889+memegoddavid@users.noreply.github.com>
2021-10-21 11:09:00 +0200

    Add files via upload

---
+++ Kontrolle22.java
@@ -0,0 +1,25 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Kontrolle22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Bitte eine natürliche Zahleingeben: ");
+		
+		int z = sc.nextInt();
+		
+		if (z < 0) {
+			z = z*-1;
+		} 
+		
+		for (int i = 1; i < z; i++) {
+			
+		}
+			
+		
+	}
+
+}
