7bddf11da1ad9411c686877de0e4b86e43b71372
David <daviidprivat@gmail.com>
2021-11-20 18:10:56 +0100

    Moin

--- Wuerfel.java
+++ Wuerfel.java
@@ -1,14 +1,20 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Wuerfel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Wie oft soll der Würfel gewürfelt werden? ");
-		int w = sc.nextInt();
+		int anzahl = sc.nextInt();
+		
+		int[] wuerfe = new int[7];
+		
+		Random rd = new Random();
+		int s = rd.nextInt(6)+1;
 		
 	}
 
