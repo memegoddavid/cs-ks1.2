f806f19d74ff2e35e60e13895e634aa27f706520
David <68856889+memegoddavid@users.noreply.github.com>
2021-11-25 10:41:19 +0100

    Add files via upload

--- Lotto.java
+++ Lotto.java
@@ -1,28 +1,54 @@
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
-
+/*public class Lotto{
+  public static void main(String[] args){
+    Random r = new Random();
+    boolean[] numbers = new boolean[50];
+    
+    for (int i = 1;i<=6 ;i++ ) {
+      numbers[r.nextInt(49)+1]=true;
+      boolean dbl = true;
+      while (dbl==true) { 
+        
+      } 
+    } 
+    System.out.println(Arrays.toString(numbers));
+  
+  
+  
+  
+  
+    }
+  }*/
 
 public class Lotto {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args){
+  
+		Random r = new Random();
+		int[] numbers = new int[6];
 		
-		Random rd = new Random();
-		Scanner sc = new Scanner(System.in);
-		
-		int[] tipp = new int[6];
-		int[] zahlen = new int[6];
-		
-		
-			for (int i = 0; i < tipp.length; i++) {
+		for (int i = 0; i < numbers.length; i++) {
+      
+			int number = r.nextInt(49);
+			
+			for (int j = 0; j < numbers.length; j++) {
+        
+				if (numbers[j] == number) {
+          
+					number = r.nextInt(49);
+					j = 0;
+					
+				}
 				
-					System.out.println("Gib deinen " + (i+1) + ". Tipp ein: ");
-					tipp[i] = sc.nextInt();
-				
-			}
-		System.out.println(Arrays.toString(tipp));
-
-	}
-
+      }
+			
+      numbers[i] = number;
+      
+    }
+		
+    System.out.println("Lotto: " + Arrays.toString(numbers));
+    
+  }
 }
